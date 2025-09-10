5803177b4260b01fb2b9a1ef83aaa6dc8bef5ac8
khalifahnur <khalifahnur1095@gmail.com>
2025-09-10 14:09:12 +0300

    fix:about route

---
+++ src/app/lycan-international/about/page.tsx
@@ -0,0 +1,8 @@
+import Container from '@/components/About/Container'
+import React from 'react'
+
+export default function page() {
+  return (
+    <Container />
+  )
+}
---
+++ src/components/About/Container.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../LandingPage/Header'
+import About from '../LandingPage/About'
+import Footer from '../LandingPage/Footer'
+
+export default function Container() {
+  return (
+    <div>
+        <Header />
+        <About />
+        <Footer />
+    </div>
+  )
+}
