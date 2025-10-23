1dad50dec77a5400a487f5cb0ba30438bf7039fa
khalifahnur <khalifahnur1095@gmail.com>
2025-10-23 14:39:08 +0300

    fix:google verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import Landing from "@/components/landing";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <Landing />
+    <div>
+      <Head>
+        {/* Google Search Console Verification Tag */}
+        <meta
+          name="google-site-verification"
+          content="google3cbf70aa2534b55b.html"
+        />
+      </Head>
+      <Landing />
+    </div>
   );
 }
